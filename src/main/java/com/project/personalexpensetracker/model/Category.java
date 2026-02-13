861d0d5474f44bd3ab81845d2a645683d8677208
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-13 15:40:58 +0530

    chore: add Category entity in Models

---
+++ src/main/java/com/project/personalexpensetracker/model/Category.java
@@ -0,0 +1,25 @@
+package com.project.personalexpensetracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
