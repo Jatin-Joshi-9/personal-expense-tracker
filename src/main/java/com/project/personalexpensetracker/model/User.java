a6640e275953ee62f05dc881455aa649185bd5c8
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-02-13 15:42:14 +0530

    chore: add User entity in Models

---
+++ src/main/java/com/project/personalexpensetracker/model/User.java
@@ -0,0 +1,22 @@
+package com.project.personalexpensetracker.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    private String email;
+}
